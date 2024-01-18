b1653f0d9ba08c5cd2a82404a95a4f0eff265bd2
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-01-18 18:11:18 +0530

    Add files via upload

---
+++ variables.go
@@ -0,0 +1,28 @@
+package main
+import "fmt"
+func main(){
+	var name1 string="eshaa"
+	var name2="akanksha"
+	var name3 string
+	fmt.Println(name1,name2,name3)
+
+	name1="Eshaa"
+	name3="Akshay"
+	fmt.Println(name1,name2,name3)
+	name4:="Reema" //Another way of defining datatype
+	fmt.Println(name4)
+
+	var number1 int=18
+	var number2=20
+	number3:=21
+	fmt.Println(number1,number2,number3)
+
+	var num1 int8=25 //integer containing 8 bits
+	fmt.Println(num1)
+
+	var num4 float32=14.56
+	var num5 float64=52.1111222
+	num6:=45.45
+	fmt.Println(num4,num5,num6)
+
+}
