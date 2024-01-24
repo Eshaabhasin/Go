3d098873e291540117b04064b8bf60f4dfbe1fd9
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-01-24 22:45:20 +0530

    Add files via upload

---
+++ array.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main(){
+	var ages [3] int=[3] int{9,7,8}
+	var age=[2] int{8,7}
+	fmt.Println(ages)
+	fmt.Println(age)
+	fmt.Println(ages,len(ages))
+	fmt.Println(age,len(age))
+}
